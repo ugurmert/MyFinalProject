1730a81f413447f99f7328ad82b360062253bc76
ugurmert <kdrugurmrt@gmail.com>
2021-03-24 02:48:12 +0300

    Add project files.

---
+++ DataAccess/Concrete/EntityFramework/EfProductDal.cs
@@ -0,0 +1,62 @@
+﻿using DataAccess.Abstract;
+using Entities.Concrete;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+
+namespace DataAccess.Concrete.EntityFramework
+{
+    public class EfProductDal : IProductDal
+    {
+        public void Add(Product entity)
+        {
+            //IDisposable pattern implementation of c#
+            //burada using kullanarak kullanımı pahalı olan context'i, blok bitince çöp toplayıcı ile bellekten hemen at demiş oluyoruz.
+            using (NorthwindContext context = new NorthwindContext())
+            {
+                var addedEntity = context.Entry(entity);    //referansı yakala
+                addedEntity.State = EntityState.Added;  //ekleme durumu olarak set et
+                context.SaveChanges();  //ekle şimdi
+            }
+        }
+
+        public void Delete(Product entity)
+        {
+            using (NorthwindContext context = new NorthwindContext())
+            {
+                var deletedEntity = context.Entry(entity);    //referansı yakala
+                deletedEntity.State = EntityState.Deleted;  //silme durumu olarak set et
+                context.SaveChanges();  //sil şimdi
+            }
+        }
+
+        public Product Get(Expression<Func<Product, bool>> filter)
+        {
+            using (NorthwindContext context = new NorthwindContext())
+            {
+                return context.Set<Product>().SingleOrDefault(filter);
+            }
+        }
+
+        public List<Product> GetAll(Expression<Func<Product, bool>> filter = null)
+        {
+            using (NorthwindContext context = new NorthwindContext())
+            {
+                return filter == null ? context.Set<Product>().ToList() : context.Set<Product>().Where(filter).ToList();
+            }
+        }
+
+        public void Update(Product entity)
+        {
+            using (NorthwindContext context = new NorthwindContext())
+            {
+                var updatedEntity = context.Entry(entity);    //referansı yakala
+                updatedEntity.State = EntityState.Modified;  //güncelleme durumu olarak set et
+                context.SaveChanges();  //güncelle şimdi
+            }
+        }
+    }
+}
---
+++ DataAccess/Concrete/EntityFramework/NorthwindContext.cs
@@ -0,0 +1,27 @@
+﻿using Entities.Concrete;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DataAccess.Concrete.EntityFramework
+{
+    //Context : Db tabloları ile proje classlarını bağlamak
+    public class NorthwindContext : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            //Parantez içerisinde database server adresi yazılır.
+            //Ancak bizim kendi bilgisayarımızda sql server'a ulaşacağımız için Server=(localdb)\MSSQLLocalDB; yazarız.
+            //Database=Northwind; ile database adını belirtiriz.
+            //Trusted_Connection=true ile sql server'a kullanıcı adı, şifre gerektirmeden bağlanmayı etkin kılar.
+            //Trusted_Connection=true kısmında kullanıcı adı, şifre de kullanılabilir.
+            optionsBuilder.UseSqlServer(@"Server=(localdb)\mssqllocaldb;Database=Northwind;Trusted_Connection=true");
+        }
+
+        //DbSet<> ile class'ı veritabanındaki tabloya bağlama işlemi aşağıdaki şekilde yapıldı.
+        public DbSet<Product> Products { get; set; }
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Customer> Customers { get; set; }
+    }
+}
